1eea27bcd37fe5e14933e559cad7efff1e899467
Izuddeen Azim <izuddeenazim@gmail.com>
2023-05-02 11:04:47 +0800

    Initial commit

---
+++ test.py
@@ -0,0 +1,17 @@
+# class Dog:
+#     def __init__(self):
+#         self.temperament = "loyal"
+#
+#
+# class Labrador(Dog):
+#     def __init__(self):
+#         super().__init__()
+#         # self.temperament = "gentle"
+#
+#
+# doggo = Dog()
+# print(f"A dog is {doggo.temperament}")
+#
+# sparky = Labrador()
+# print(f"Sparky is {sparky.temperament}")
+print(len("95637+12"))
